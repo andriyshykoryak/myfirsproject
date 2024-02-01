3987931cdce3379de89caee705e0c6a99d75993e
Androv14 <shykoryak1111@gmail.com>
2024-02-01 07:05:22 +0200

    SectionAbout

--- viceversa/urls.py
+++ viceversa/urls.py
@@ -19,5 +19,6 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('home/',views.home)
+    path('home/',views.home),
+    path('about/',views.about)
 ]
